cfe2b2ecc78baabd4d9477c14da44921e6f987cc
Jack Maney <jackmaney@gmail.com>
2015-03-12 12:29:45 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+from _version import __version__
+
+try:
+    with open("README.rst") as f:
+        long_description = f.read()
+except IOError:
+    long_description = ""
+
+try:
+    with open("requirements.txt") as f:
+        requirements = [x for x in [y.strip() for y in f.readlines()] if x]
+except IOError:
+    requirements = []
+
+setuptools.setup(
+    name="interrogator",
+    license="MIT",
+    author="Jack Maney",
+    author_email="jackmaney@gmail.com",
+    url="https://github.com/jackmaney/interrogator.git",
+    version=__version__,
+    install_requires=requirements,
+    packages=setuptools.find_packages(),
+    long_description=long_description
+)
